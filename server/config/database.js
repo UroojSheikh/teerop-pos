300ee053ddbd8e9a47157a9533a5b02ee2622a2a
Urooj Sheikh <s.urooj197@gmail.com>
2026-08-18 14:11:20 +0500

    fix: add SSL connection requirement for cloud database

--- server/config/database.js
+++ server/config/database.js
@@ -4,6 +4,12 @@ require('dotenv').config();
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   logging: false, // Set to console.log to see SQL queries
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 });
 
 module.exports = sequelize;
